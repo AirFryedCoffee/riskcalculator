423ae1863a815702d2bd9effe0c20b452abbe0ab
AirFryedCoffee <jpbryant6@gmail.com>
2024-01-06 23:40:13 -0800

    Working as Expected, Clean Up Soon
    Working as expected, will make some additional changes, create test cases, to make sure that the program operates as intended.

--- riskcalculator copy/stockriskcaclulator/src/UserInputJFrame.java
+++ riskcalculator copy/stockriskcaclulator/src/UserInputJFrame.java
@@ -13,7 +13,7 @@ public class UserInputJFrame extends JFrame {
     private JTextField stopOutPrice;
     private JLabel stopOutPriceLabel;
     private JComboBox riskPercentageAmount;
-    private JLabel riskPercentageAmountLabel;
+    private JLabel riskPercentageAmountLabel; 
     private JButton calculateButton;
     float num3;
 
@@ -21,6 +21,7 @@ public class UserInputJFrame extends JFrame {
     {
         setTitle("Risk Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
 
         panel = new JPanel();
         panel.setLayout(null);
@@ -69,8 +70,8 @@ public class UserInputJFrame extends JFrame {
                             if (checkRiskPercentageWithAccBalance(Integer.parseInt(accountBalanceField.getText()), Float.parseFloat(stockTradingPrice.getText()), Float.parseFloat(stopOutPrice.getText())))
                             {
                                 JOptionPane.showMessageDialog(null, "Everything is still working as expected", "Validation Result", JOptionPane.INFORMATION_MESSAGE);
-                                float totalRiskAmount = calculateRisk(Integer.parseInt(accountBalanceField.getText()), Float.parseFloat((String) riskPercentageAmount.getSelectedItem()));
-                                JOptionPane.showMessageDialog(null, "Your risk is " + totalRiskAmount, "Validation Result", JOptionPane.INFORMATION_MESSAGE);
+                                float theFinalTrade = calculateSomething(Integer.parseInt(accountBalanceField.getText()), Float.parseFloat(stockTradingPrice.getText()), Float.parseFloat(stopOutPrice.getText()), Float.parseFloat((String) riskPercentageAmount.getSelectedItem()));
+                                JOptionPane.showMessageDialog(null, "You should purchase " + theFinalTrade + " shares", "Validation Result", JOptionPane.INFORMATION_MESSAGE);
                             }
                             else
                             {
@@ -122,7 +123,7 @@ public class UserInputJFrame extends JFrame {
     {
         return num1 > num2;
     }
-    //Just testing this Function, change functionality later.
+
     private boolean checkRiskPercentageWithAccBalance(int num1, float num2, float num3)
     {
         return num1 > num2 * 10 && num2 > num3;
@@ -133,8 +134,21 @@ public class UserInputJFrame extends JFrame {
         float result = (riskAmount / 100) * accBal;
         return result;
     }
+    private float calculateSomething(int num1, double num2, double num3, double num4) {
+        double riskPercentage = num4 / 100;
+        double riskDollarAmount = num1 * riskPercentage;
+        double currentRisk = 0;
+        int numberOfShares = 0;
+        int lastNumberOfShares = 0;
 
+        while (currentRisk < riskDollarAmount) {
+            currentRisk = (num2 * numberOfShares) - (num3 * numberOfShares);
+            lastNumberOfShares = numberOfShares;
+            numberOfShares +=10;
+        }
 
+        return lastNumberOfShares;
+    }
 
 
     public static void main(String[] args) {
